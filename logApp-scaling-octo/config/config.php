c67574d941351d8fa6f68d35eb9662dc462e81b7
pensense <72497373+pensense@users.noreply.github.com>
2021-06-02 17:05:22 +0800

    Add files via upload

---
+++ logApp-scaling-octo/config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'guestbook-login.php');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6416540');
+	define('DB_PASS', 'HzdRi9JVAF');
+	define('DB_NAME', 'sql6416540');
